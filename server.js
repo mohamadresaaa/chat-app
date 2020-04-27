2000a2ea763dff9632f952d4d30dc41aaf91811d
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-04-27 15:34:25 +0430

    setup routes

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const express = require("express")
 const http = require("http")
 const container = require("./container")
 
-container.resolve(() => {
+container.resolve((users) => {
     const app = setupExpress()
 
     const setupExpress = () => {
         const server = http.createServer(express())
         server.listen(3000, () => console.log("port 3000"))
     }
+
+    // Setup router
+    const router = require("express-promise-router")()
+    users.setRouting(router)
+
+    // Use router
+    app.use(router)
 })
